128e098c50bda9c83c62d39aee7ef516f20155bd
Marcelo-Flores-Solis <mfloresso@unsa.edu.pe>
2026-01-04 20:14:13 -0500

    base de dato hospedada

---
+++ crear_tabla.py
@@ -0,0 +1,41 @@
+import mysql.connector
+
+config = {
+    'host': 'PEGA_AQUI_TU_MYSQLHOST',     
+    'user': 'PEGA_AQUI_TU_MYSQLUSER',      
+    'password': 'PEGA_AQUI_TU_MYSQLPASSWORD', 
+    'database': 'PEGA_AQUI_TU_MYSQLDATABASE', 
+    'port': 3306                                
+}
+
+try:
+    # 1. Conectamos
+    print("Conectando a la nube...")
+    conexion = mysql.connector.connect(**config)
+    cursor = conexion.cursor()
+
+    # 2. El código SQL para crear la tabla
+    sql = """
+    CREATE TABLE IF NOT EXISTS mensajes (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nombre VARCHAR(100) NOT NULL,
+        email VARCHAR(100) NOT NULL,
+        mensaje TEXT NOT NULL,
+        fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    """
+
+    cursor.execute(sql)
+    print("tabla creada")
+
+    cursor.execute("INSERT INTO mensajes (nombre, email, mensaje) VALUES ('Admin', 'test@railway.app', 'Base de datos conectada OK')")
+    conexion.commit()
+    print("Mensaje de prueba insertado.")
+
+except Exception as e:
+    print(f"Error: {e}")
+
+finally:
+    if 'conexion' in locals() and conexion.is_connected():
+        cursor.close()
+        conexion.close()
